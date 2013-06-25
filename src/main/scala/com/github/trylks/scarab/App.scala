bc1e27af82d2a87374fe99ec95632299509ac770
trylks <trylks@gmail.com>
2013-06-25 18:28:12 +0200

    Initializing
    With all those things from the archetype... and one line more in the
    gitignore

---
+++ src/main/scala/com/github/trylks/scarab/App.scala
@@ -0,0 +1,15 @@
+package com.github.trylks.scarab
+
+/**
+ * @author trylks
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
