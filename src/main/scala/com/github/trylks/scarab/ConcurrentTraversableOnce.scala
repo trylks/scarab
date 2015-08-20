9cbb37b08f59ac49d1ad59b31fd51899af86fbbc
trylks <trylks@live.com>
2015-08-20 05:39:00 +0200

    strange diff...

--- src/main/scala/com/github/trylks/scarab/ConcurrentTraversableOnce.scala
+++ src/main/scala/com/github/trylks/scarab/ConcurrentTraversableOnce.scala
@@ -5,7 +5,7 @@ import ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 
 class ConcurrentTraversableOnce[A, T <: TraversableOnce[A]](val innerTraversable: T) {
-    def cmap[R](function: A => R) = {
-        innerTraversable.map(x => future { function(x) }).toIterator.map(Await.result(_, Duration.Inf))
-    }
+  def cmap[R](function: A => R) = {
+    innerTraversable.map(x => Future { function(x) }).toIterator.map(Await.result(_, Duration.Inf))
+  }
 }
